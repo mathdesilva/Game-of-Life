929c1d6d07050fd1cbea722d74cf6b6806127b18
Matheus de Andrade Silva <matheus.mas132@hotmail.com>
2019-05-27 22:30:37 -0300

    readme update and help update

--- src/prints.cpp
+++ src/prints.cpp
@@ -26,12 +26,12 @@ void print_help( void )
 			  << "  --help               Prints this help text.\n\n"
 			  << "  --imgdir <path>      Specify directory where output images are written to.\n\n"
 			  << "  --maxgen <num>       Maximum number of generations to simulate.\n\n"
-			  << "  --fps <num>          Number of generations presented per second.\n\n"
-			  << "  --blocksize <num>    Pixel size of a cell. Default = 5.\n\n"
-			  << "  --bkgcolor <color>   Color name for the background. Default GREEN.\n\n"
-			  << "  --alivecolor <color> Color name for representing alive cells. Default RED.\n\n"
+			  << "  --fps <num>          Number of generations presented per second. Default = 2\n\n"
+			  << "  --blocksize <num>    Pixel size of a cell. Default = 10.\n\n"
+			  << "  --bkgcolor <color>   Color name for the background. Default = GREEN.\n\n"
+			  << "  --alivecolor <color> Color name for representing alive cells. Default = BLUE.\n\n"
 			  << "  --outfile <filename> Write the text representation of the simulation to the given filename.\n\n"
-			  << "  --rule <code>        Specify evolution rule using Bx/Sx code, B for born and S for survives.\n\n"
+			  << "  --rule <code>        Specify evolution rule using B-/S- code, B for born and S for survives. Default = B3/S23\n\n"
 			  << std::endl << std::endl
 			  << "Available colors are:\n    BLACK BLUE CRIMSON DARK_GREEN DEEP_SKY_BLUE DODGER_BLUE\n    GREEN LIGHT_BLUE LIGHT_GREY LIGHT_YELLOW RED STEEL_BLUE\n    WHITE YELLOW" << std::endl;
 
